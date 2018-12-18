03e217e02296d6c178f36dc368d85f3b1159bf42
Rossetti, Devin <drossetti@corelogic.com>
2018-12-18 16:27:31 -0600

    added nullable dto options

--- Forge2018Connectivity/Forge2018Connectivity/OrderCalculationDataDto.cs
+++ Forge2018Connectivity/Forge2018Connectivity/OrderCalculationDataDto.cs
@@ -7,7 +7,7 @@ namespace Forge2018Connectivity
     public class OrderCalculationDataDto
     {
         public string GSE { get; set; }
-        public object SSRFile { get; set; }
+        public bool? SSRFile { get; set; }
         public string BorrowerCreditScore { get; set; }
         public string CUScore { get; set; }
         public string LCAScore { get; set; }
@@ -15,11 +15,11 @@ namespace Forge2018Connectivity
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
-        public bool CRA { get; set; }
+        public bool? CRA { get; set; }
         public string LoanProduct { get; set; }
-        public decimal DebtToIncome { get; set; }
-        public decimal InterestRate { get; set; }
-        public decimal AppraisedValue { get; set; }
+        public decimal? DebtToIncome { get; set; }
+        public decimal? InterestRate { get; set; }
+        public decimal? AppraisedValue { get; set; }
         public string FormType { get; set; }
         public string GAARScore { get; set; }
         public string LoanNumber { get; set; }
