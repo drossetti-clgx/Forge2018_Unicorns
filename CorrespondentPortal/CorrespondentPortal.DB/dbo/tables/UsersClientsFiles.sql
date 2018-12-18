7c5321142037faca61cb6493e27e3c4e0830ca9f
Rossetti, Devin <drossetti@corelogic.com>
2018-12-18 15:36:42 -0600

    adding new sql tables

---
+++ CorrespondentPortal/CorrespondentPortal.DB/dbo/tables/UsersClientsFiles.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UsersClientsFiles]
+(
+	[UserID] INT NOT NULL PRIMARY KEY, 
+    [ClientID] INT NOT NULL, 
+    [FileGuid] UNIQUEIDENTIFIER NOT NULL, 
+    [Status] INT NOT NULL, 
+    [AcceptedDt] DATETIME NULL, 
+    [AssignedDt] DATETIME NULL, 
+    [BaseScore] INT NULL
+)
