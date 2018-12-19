65149fb7e5dc583167d138ddd184f3f08a29dddc
Rossetti, Devin <drossetti@corelogic.com>
2018-12-19 07:57:41 -0600

    what I have

---
+++ CorrespondentPortal/CorrespondentPortal.WebApi/Controllers/DummyCallbackUrlEndpointsController.cs
@@ -0,0 +1,114 @@
+﻿using CorrespondentPortal.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorrespondentPortal.WebApi.Controllers
+{
+    public class DummyCallbackUrlEndpointsController : Controller
+    {
+        public DummyCallbackUrlEndpointsController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("Callback/GetBankOneUrl")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public ClientCallbackInfoDto GetBankOneCallbackInformation()
+        {
+            return new ClientCallbackInfoDto
+            {
+                BorrowerCreditScore = new BorrowerCreditScore
+                {
+                    Min = 1,
+                    Max = 750,
+                    Weight = 20
+                },
+                CRA = new Cra
+                {
+                    YearTerm = 15,
+                    RateTerm = "Adjustable",
+                    Weight = 10
+                },
+                DebtToIncome = new DebtToIncome
+                {
+                    Min = 1,
+                    Max = 5,
+                    Weight = 10
+                },
+                FannieCU = new FannieCU
+                {
+                    Min = 1,
+                    Max = 90,
+                    Weight = 10
+                }, 
+                FreddieLCA = new FreddieLCA
+                {
+                    Min = 1,
+                    Max = 50,
+                    Weight = 10
+                },
+                GaarScore = new GaarScore
+                {
+                    Min = 1,
+                    Max = 50,
+                    Weight = 10
+                },
+                InterestRate = new InterestRate
+                {
+                    Min = 10.25,
+                    Max = 50.86,
+                    Weight = 10
+                },
+                LoanAmount = new LoanAmount
+                {
+                    Min = 100500,
+                    Max = 500000,
+                    Weight = 10
+                },
+                LoanToValue = new LoanToValue
+                {
+                    Min = 1,
+                    Max = 100,
+                    Weight = 10
+                }
+            };
+        }
+
+        [HttpGet]
+        [Route("Callback/GetBankOneUrl")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public ClientCallbackInfoDto GetBankTwoCallbackInformation()
+        {
+            return new ClientCallbackInfoDto
+            {
+
+            };
+        }
+
+        [HttpGet]
+        [Route("Callback/GetBankOneUrl")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public ClientCallbackInfoDto GetBankThreeCallbackInformation()
+        {
+            return new ClientCallbackInfoDto
+            {
+
+            };
+        }
+
+        [HttpGet]
+        [Route("Callback/GetBankOneUrl")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public ClientCallbackInfoDto GetBankFourCallbackInformation()
+        {
+            return new ClientCallbackInfoDto
+            {
+
+            };
+        }
+    }
+}
---
+++ CorrespondentPortal/CorrespondentPortal.WebApi/Models/ClientCallbackInfoDto.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorrespondentPortal.WebApi.Models
+{
+    public class ClientCallbackInfoDto
+    {
+        public LoanToValue LoanToValue { get; set; }
+        public DebtToIncome DebtToIncome { get; set; }
+        public GaarScore GaarScore { get; set; }
+        public BorrowerCreditScore BorrowerCreditScore { get; set; }
+        public FannieCU FannieCU { get; set; }
+        public FreddieLCA FreddieLCA { get; set; }
+        public Cra CRA { get; set; }
+        public InterestRate InterestRate { get; set; }
+        public LoanAmount LoanAmount { get; set; }
+    }
+
+    public class LoanToValue
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class DebtToIncome
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class GaarScore
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class BorrowerCreditScore
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class FannieCU
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class FreddieLCA
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class Cra
+    {
+        public int YearTerm { get; set; }
+        public string RateTerm { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class InterestRate
+    {
+        public double Min { get; set; }
+        public double Max { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class LoanAmount
+    {
+        public double Min { get; set; }
+        public double Max { get; set; }
+        public int Weight { get; set; }
+    }
+}
